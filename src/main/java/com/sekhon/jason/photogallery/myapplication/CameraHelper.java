009676026d36f817a07f1ec8359c71316c4799d7
JasonSekhon <jason_2s@hotmail.com>
2018-10-11 15:41:04 -0700

    Async tasks for queries

--- src/main/java/com/sekhon/jason/photogallery/myapplication/CameraHelper.java
+++ src/main/java/com/sekhon/jason/photogallery/myapplication/CameraHelper.java
@@ -13,9 +13,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.BaseColumns;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -35,8 +35,8 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 public class CameraHelper extends AppCompatActivity {
 
@@ -54,6 +54,16 @@ public class CameraHelper extends AppCompatActivity {
     private LocationManager lm;
     private double longitude;
     private double latitude;
+
+    private static class QueryParams {
+        String selection;
+        String[] selectionArgs;
+
+        QueryParams(String selection, String[] selectionArgs) {
+            this.selection = selection;
+            this.selectionArgs = selectionArgs;
+        }
+    }
 
     public CameraHelper(Context context, Activity a) {
         sDateParse = new SimpleDateFormat("yyyyMMdd");
@@ -119,6 +129,17 @@ public class CameraHelper extends AppCompatActivity {
         this.currentPhotoPath = currentPhotoPath;
     }
 
+    private class FetchQuery extends AsyncTask<QueryParams, Void, Cursor>{
+
+        @Override
+        protected Cursor doInBackground(QueryParams... params) {
+            Cursor c = db.query(PhotoGalleryDBBuilder.PhotoGalleryEntry.TABLE_NAME,
+                    null, params[0].selection, params[0].selectionArgs, null,
+                    null, null);
+            return c;
+        }
+    }
+
     public ArrayList<String> populateGallery(Date minDate, Date maxDate, double minlon, double minlat, double maxlon, double maxlat, String caption){
         File file = new File(Environment.getExternalStorageDirectory()
                 .getAbsolutePath(), "/Android/data/com.sekhon.jason.photogallery/files/Pictures");
@@ -132,25 +153,39 @@ public class CameraHelper extends AppCompatActivity {
                         + "instr(" + PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_CAPTION + ", ?) > 0";
                 DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
                 String[] selectionArgs = { df.format(minDate), df.format(maxDate), minlat+"", maxlat+"", minlon+"", maxlon+"", caption };
-                Cursor c = db.query(PhotoGalleryDBBuilder.PhotoGalleryEntry.TABLE_NAME,
-                        null, selection, selectionArgs, null,
-                        null, null);
-                while(c.moveToNext()) {
-                    photoGallery.add(c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_PATH)));
+                QueryParams qp = new QueryParams(selection, selectionArgs);
+                FetchQuery fetchTask = new FetchQuery();
+                try {
+                    Cursor c = fetchTask.execute(qp).get();
+                    while(c.moveToNext()) {
+                        photoGallery.add(c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_PATH)));
+                    }
+                    c.close();
+                    return photoGallery;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
                 }
-                c.close();
             } else{
                 String selection = PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_LATITUDE + " BETWEEN ? AND ? AND "
                     + PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_LONGITUDE + " BETWEEN ? AND ? AND "
                     + "instr(" + PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_CAPTION + ", ?) > 0";
                 String[] selectionArgs = { minlat+"", maxlat+"", minlon+"", maxlon+"", caption };
-                Cursor c = db.query(PhotoGalleryDBBuilder.PhotoGalleryEntry.TABLE_NAME,
-                        null, selection, selectionArgs, null,
-                        null, null);
-                while(c.moveToNext()) {
-                    photoGallery.add(c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_PATH)));
+                QueryParams qp = new QueryParams(selection, selectionArgs);
+                FetchQuery fetchTask = new FetchQuery();
+                try {
+                    Cursor c = fetchTask.execute(qp).get();
+                    while(c.moveToNext()) {
+                        photoGallery.add(c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_PATH)));
+                    }
+                    c.close();
+                    return photoGallery;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
                 }
-                c.close();
             }
         }
         return photoGallery;
@@ -164,22 +199,27 @@ public class CameraHelper extends AppCompatActivity {
         String caption = "";
         String[] selectionArgs = {path};
         if (path != null) {
-            Cursor c = db.query(PhotoGalleryDBBuilder.PhotoGalleryEntry.TABLE_NAME,
-                    null, PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_PATH + " = ?",
-                    selectionArgs, null,
-                    null, null);
-            while (c.moveToNext()) {
-                date = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_DATE));
-                lon = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_LONGITUDE));
-                lat = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_LATITUDE));
-                caption = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_CAPTION));
+            QueryParams qp = new QueryParams(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_PATH + " = ?", selectionArgs);
+            FetchQuery fetchTask = new FetchQuery();
+            try {
+                Cursor c = fetchTask.execute(qp).get();
+                while (c.moveToNext()) {
+                    date = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_DATE));
+                    lon = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_LONGITUDE));
+                    lat = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_LATITUDE));
+                    caption = c.getString(c.getColumnIndexOrThrow(PhotoGalleryDBBuilder.PhotoGalleryEntry.COLUMN_NAME_CAPTION));
+                    imageDate.setText(date);
+                    imageLon.setText(String.format("Lon: %s", lon));
+                    imageLat.setText(String.format("Lat: %s", lat));
+                    imageCaption.setText(caption);
+                }
+                c.close();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
-            c.close();
         }
-        imageDate.setText(date);
-        imageLon.setText(String.format("Lon: %s", lon));
-        imageLat.setText(String.format("Lat: %s", lat));
-        imageCaption.setText(caption);
     }
 
     public void takePicture(View v, Activity a) {
@@ -230,13 +270,15 @@ public class CameraHelper extends AppCompatActivity {
 
     public ArrayList<String> filterSortGallery(String startDate, String endDate, double minlon,
                                                double minlat, double maxlon, double maxlat, String caption, TextView filterDate){
-        try {
-            Date parsedStartDate = new SimpleDateFormat("yyyy/MM/dd").parse(startDate);
-            Date parsedEndDate = new SimpleDateFormat("yyyy/MM/dd").parse(endDate);
-            filterDate.setText(String.format("%s\n%s", startDate, endDate));
-            return populateGallery(parsedStartDate, parsedEndDate, minlon, minlat, maxlon, maxlat, caption);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        if(startDate != null) {
+            try {
+                Date parsedStartDate = new SimpleDateFormat("yyyy/MM/dd").parse(startDate);
+                Date parsedEndDate = new SimpleDateFormat("yyyy/MM/dd").parse(endDate);
+                filterDate.setText(String.format("%s\n%s", startDate, endDate));
+                return populateGallery(parsedStartDate, parsedEndDate, minlon, minlat, maxlon, maxlat, caption);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
         return populateGallery(null, null, minlon, minlat, maxlon, maxlat, caption);
     }
